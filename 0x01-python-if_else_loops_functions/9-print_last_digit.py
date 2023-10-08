353f21a03f575366a1e20fbaa9b3ad20293bb419
AtfiWiam <wiam.atfi@mail.com>
2023-10-08 06:58:26 -0700

    0x01-python-if_else_loops_functions

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,7 +2,7 @@
 def print_last_digit(number):
     if number < 0:
         last_digit = -number % 10
-        last_digit = ldigit
+        last_digit = last_digit
     else:
         last_digit = number % 10
     print(last_digit, end="")
