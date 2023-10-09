77b933f84eec720bf35c046c447a4c09b5e2cd5e
AtfiWiam <wiam.atfi@mail.com>
2023-10-09 05:14:16 -0700

    0x03-python-data_structures

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     lenlist = len(my_list) - 1
-    if (idx < 0 or idx > lenlist:
+    if (idx < 0 or idx > lenlist):
         return (my_list)
     else:
         new_list = my_list[:]
