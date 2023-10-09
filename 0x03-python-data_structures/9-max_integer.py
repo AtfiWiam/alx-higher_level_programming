a6d7da4231a212edcf685c18675dc04bb94b3045
AtfiWiam <wiam.atfi@mail.com>
2023-10-09 05:35:11 -0700

    0x03-python-data_structures

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    t = len(my_list)
-    if t == 0:
+    tam = len(my_list)
+    if tam == 0:
         return None
     else:
         l = my_list[0]
-        for iter in range(1, t):
+        for iter in range(1, tam):
             if my_list[iter] > l:
                 l = my_list[iter]
     return l
