179e945af5fc9dca1e0df0d9bf26bfaf6917308a
AtfiWiam <wiam.atfi@mail.com>
2023-10-12 05:38:44 -0700

    0x04-python-more_data_structures

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,13 +1,29 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not roman_string or type(roman_string) != str:
+    if not roman_string:
         return 0
-    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    int_trans = 0
-    for j in range(len(roman_string)):
-        if j > 0 and roman_dict[roman_string[j]] > roman_dict[roman_string[j - 1]]:
-            int_trans += roman_dict[roman_string[j]] - 2 * \
-                        roman_dict[roman_string[j - 1]]
+    if not isinstance(roman_string, str):
+        return 0
+
+    roman_dict = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+
+    new_value = 0
+    int_value = 0
+
+    for char in roman_string:
+        value = roman_dict.get(char, 0)
+        if value > int_value:
+            new_value = new_value + value - 2 * int_value
         else:
-            int_trans += roman_dict[roman_string[j]]
-    return int_trans
+            new_value = new_value + value
+        int_value = value
+
+    return new_value
