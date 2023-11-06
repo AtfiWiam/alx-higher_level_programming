ad3ceeae8702a879cba704dc686f61d56bf062c3
AtfiWiam <wiam.atfi@mail.com>
2023-11-06 05:03:57 -0800

    0x0A-python-inheritance

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,26 +1,30 @@
 #!/usr/bin/python3
+"""class Rectangle that inherits from BaseGeometry."""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
-"""
-module with class BaseGeometry
-"""
 
 
 class Rectangle(BaseGeometry):
-    """Rectangle class that inherits from BaseGeometry"""
+    """rectangle class using BaseGeometry."""
 
     def __init__(self, width, height):
-        """Method for initialized the attrubutes"""
-        self.integer_validator("width", width)
+        """init new Rectangle.
+
+        Args:
+            width: The width of the new Rectangle.
+            height: The height of the new Rectangle.
+        """
+
+        super().integer_validator("width", width)
         self.__width = width
-        self.integer_validator("height", height)
+        super().integer_validator("height", height)
         self.__height = height
 
     def area(self):
-        """Method to redefine a area method in the parent class"""
-
+        """return the area of the rectangle."""
         return self.__width * self.__height
 
     def __str__(self):
-        """__str__ method for return the next string"""
-
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+        """return the print() and str() of rectangle obj."""
+        string = "[" + str(self.__class__.__name__) + "] "
+        string += str(self.__width) + "/" + str(self.__height)
+        return string
